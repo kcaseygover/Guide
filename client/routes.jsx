9cf30564d4f5f43ef2b40a7082b251fc724502f4
Karen Casey-Gover <Karen@Karens-MacBook-Pro.local>
2016-11-27 13:51:19 -0400

    added more routes

--- client/routes.jsx
+++ client/routes.jsx
@@ -7,12 +7,13 @@ import ListEvent from '../imports/ui/ListEvent.jsx';
 import Event from '../imports/ui/Event.jsx';
 
 import NewEvent from '../imports/ui/NewEvent.jsx';
+import Profile from '../imports/ui/Profile.jsx';
 
 
 FlowRouter.route('/', {
     name: 'root',
     action: function (){
-        //console.log("This is my root:", params);
+        console.log("This is my root:");
         ReactLayout.render(App, {content: <ListEvent />});
     }
 });
@@ -20,8 +21,8 @@ FlowRouter.route('/', {
 FlowRouter.route('/events', {
     name: 'events',
     action: function (){
-        //console.log("This is my root:", params);
-        ReactLayout.render(ListEvent, {content: <ListEvent />});
+
+      ReactLayout.render(ListEvent, {content: <ListEvent />});
     }
 });
 
@@ -29,15 +30,22 @@ FlowRouter.route('/events', {
 FlowRouter.route('/events/new', {
     name: 'events_new',
     action: function (){
-        //console.log("This is my root:", params);
+        console.log("This is my /events/new:");
         ReactLayout.render(NewEvent, {content: <NewEvent />});
+    }
+});
+FlowRouter.route('/user/:id', {
+    name: 'profile',
+    action: function (params){
+        console.log("This is my /user/:id:", params);
+        ReactLayout.render(Profile, {content: <Profile />});
     }
 });
 
 FlowRouter.route('/events/:id', {
     name: 'event_id',
     action: function (params){
-
+ console.log("This is my /events/:id:", params);
         ReactLayout.render(Event, {content: <Event {...params} />});
     }
 });
